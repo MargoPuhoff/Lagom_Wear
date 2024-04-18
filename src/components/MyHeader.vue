699b98b54b3863ed36560bd6943df97c1071c309
Margo Puhoff <5334936-MargoNiemand@users.noreply.replit.com>
2024-04-18 16:10:02 +0000

    Зарегистрировала компоненты

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Я header</div>
+</template>
+
+<script>
+  export default{
+    
+  }
+</script>
+
+<style scoped>
+  
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,12 +1,21 @@
 <template>
+  <my-header/>
   <div>
     Магазин Lagom Wear
   </div>
+  <my-footer/>
 </template>
 
 <script>
+import MyFooter from "@/components/MyFooter.vue"
+import MyHeader from "@/components/MyHeader.vue"
+  
 export default{
-    
+  
+  components:{
+    MyFooter, MyHeader
+  } 
+  
 }
 </script>
 
