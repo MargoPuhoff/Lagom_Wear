a6949b7f1b5ac3ac449294f97e180c0b9fe170d2
Margo Puhoff <5334936-MargoNiemand@users.noreply.replit.com>
2024-04-28 16:55:52 +0000

    Добавила иконки избранного

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -6,19 +6,34 @@
     </div>
       
     <div class="card__item-info">
-      <p>Размер</p>
-      <b>{{ title }}</b>
-      <p>{{ price }}</p>
+      <div class="item-info">
+        <p>Размер</p>
+        <b>{{ title }}</b>
+        <p>{{ price }}</p>
+      </div>
+      
+      <div class="item-icon">
+        <img
+          @click="onClickFavorite"
+          width="50" height="50" 
+          :src="!isFavorite ? 'https://img.icons8.com/bubbles/50/hearts.png' : 'https://img.icons8.com/bubbles/50/filled-like.png'"/>
+      </div>
     </div>
       
   </div>
 </template>
 
 <script setup>
+  const onClickFavorite = () => {
+    alert("Товар добавлен в избранное!")
+  }
+  
   defineProps({
     imgUrl: String,
     title: String,
-    price: Number
+    price: Number,
+    isFavorite: Boolean,
+    onClickFavorite: Function
   })
 </script>
 
@@ -33,6 +48,13 @@
 
   .card__item-info{
     padding-top: 1.5rem;
+    display: flex;
+    justify-content: space-between;
     line-height: 20px;
   }
+
+  .item-icon{
+    cursor: pointer;
+  }
+
 </style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,15 +1,18 @@
 <template>
 
     <div class="card__list-item">
-      <CardItem 
+      <CardItem
+        :isFavorite="false"
         title="Название"
         :price="1500"
         imgUrl="https://img.freepik.com/free-photo/beautiful-blonde-girl-white-oversized-tshirt-blue-jeans-posing-gray-background_89887-922.jpg?t=st=1714319485~exp=1714323085~hmac=f891ac795b654190a95cefbcba013fe3928de1f620c6a804d1b1b3f80d176c71&w=740"/>
       <CardItem
+        :isFavorite="false"
         title="Название"
         :price="1500"
         imgUrl="https://img.freepik.com/free-photo/beautiful-blonde-girl-white-oversized-tshirt-blue-jeans-posing-gray-background_89887-899.jpg?t=st=1714309911~exp=1714313511~hmac=b30f91f5ac58c62553188564dced5a60c8a5d769cda0214f0fabf185ac5bbcc9&w=740"/>
       <CardItem
+        :isFavorite="true"
         title="Название"
         :price="1500"
         imgUrl="https://img.freepik.com/free-photo/man-posing-with-sunglasses-medium-shot_23-2149409751.jpg?t=st=1714309960~exp=1714313560~hmac=634138c5610d3f23cae4f0c36f11c36434cc85209159ec69d971f66c5b8a595d&w=740"/>
