9167b3e4aaec949b8be414ff6ecfc53dd759d347
Margo Puhoff <5334936-MargoNiemand@users.noreply.replit.com>
2024-05-26 12:28:35 +0000

    Сделала адаптивное меню для header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,26 +1,46 @@
 <template>
   <div class="header">
-    <div>Логотип</div>
+    <div v-if="!open">Логотип</div>
     
     <div>
-      <ul>
+      <ul class="menu__desktop">
         
         <router-link to="/"><li>Главная</li></router-link>
         <router-link to="/catalog"><li>Одежда</li></router-link>
-         <li><img src="@/img/icons/contact.svg"></li>
-        <li><img width="50" height="50" src="https://img.icons8.com/bubbles/50/in-transit.png"/></li>
-        <!--<li><img src="@/img/icons/search.svg"/></li>-->
+        <router-link to="/contact"><li><img src="@/img/icons/contact.svg"></li></router-link>
+        <router-link to="/delivery"><li><img width="50" height="50" src="https://img.icons8.com/bubbles/50/in-transit.png"/></li></router-link>
         <router-link to="/favorite"><li>
           <img width="50" height="50" src="https://img.icons8.com/bubbles/50/likes-folder.png"/>
         </li></router-link>
         
       </ul>
+      
+      <div class="menu__mobile">
+        <a href="#" 
+          class="header__menu"
+          @click="openMenu">Меню</a>
+        <ul v-if="open">
+          <router-link to="/"><li>Главная</li></router-link>
+          <router-link to="/catalog"><li>Одежда</li></router-link>
+          <router-link to="/contact"><li>Контакты</li></router-link>
+          <router-link to="/delivery"><li>Доставка</li></router-link>
+          <router-link to="/favorite"><li>Избранное</li></router-link>
+        </ul>
+      </div>
+      
     </div>
   </div>
 </template>
 
 <script setup>
+  import {ref} from "vue"
+  
+  const open = ref(false)
   const emit = defineEmits(['openFavorite']);
+
+  const openMenu = () => {
+    open.value = !open.value;
+  };
 </script>
 
 <style scoped>
@@ -36,11 +56,22 @@
     background: rgba(113, 65, 51, 1);
   }
   
-  ul{
+  .menu__desktop{
     display: flex;
     flex-direction: row;
     align-items: center;
   }
+  
+  .menu__mobile{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+   .menu__mobile li{
+      padding: 0.5rem;
+   }
   
   li{
     padding-left: 1.8rem;
@@ -49,6 +80,25 @@
   }
 
   a{
+    color: white;
     text-decoration-line: none;
   }
+
+  .header__menu{
+    display: none;
+  }
+  
+/* Адаптив для меню */
+  
+  @media screen and (max-width: 750px) {
+    
+    .menu__desktop{
+      display: none;
+    }
+    
+    .header__menu{
+      display: block;
+    }
+  }
+  
 </style>
